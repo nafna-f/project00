f50329d7e89d429acf7e5a5253a65d211899dd4e
Ben Rudinski <105463867+Tuvalu23@users.noreply.github.com>
2024-10-31 11:58:30 -0400

    began init py with imports of Flask, database, and routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,21 @@
-# prepare to feast your eyes upon the greatest python code ever served !
+# imports
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+
+#inialize our flask app and config
+app = Flask(__name__)
+app.config.from_object(Config)
+
+# initialize our database of sql alchemy
+db = SQLAlchemy(app)
+
+# import our routes and models for the app
+from app import routes, models
+from app import db
+
+class User (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+
+
